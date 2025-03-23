2744c5612d818b4a97eb66bfead95d52e6c734d3
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2025-03-23 21:36:14 +0000

    generate fake todos data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,18 +4,26 @@ import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.createMany({
+  // Generate fake data for Todo model
+  await prisma.todo.createMany({
     data: Array.from({ length: 25 }).map(() => ({
-      email: faker.internet.email(),
-      name: faker.internet.username(),
-      address: {
-        street: faker.location.street(),
-        city: faker.location.city(),
-        state: faker.location.state(),
-        zip: faker.location.zipCode(),
-      },
+      title: faker.lorem.sentence(),
+      body: faker.lorem.paragraph(),
     })),
   });
+  // Generate fake data for User model
+  // await prisma.user.createMany({
+  //   data: Array.from({ length: 25 }).map(() => ({
+  //     email: faker.internet.email(),
+  //     name: faker.internet.username(),
+  //     address: {
+  //       street: faker.location.street(),
+  //       city: faker.location.city(),
+  //       state: faker.location.state(),
+  //       zip: faker.location.zipCode(),
+  //     },
+  //   })),
+  // });
 }
 
 main()
